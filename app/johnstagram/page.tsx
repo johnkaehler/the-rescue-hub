b82f7601527b137e0c755fdac151053eaa606821
John Kaehler <johnkaehler92@gmail.com>
2025-02-02 17:37:18 -0700

    checking if this deploys in prod

--- app/johnstagram/page.tsx
+++ app/johnstagram/page.tsx
@@ -6,14 +6,11 @@ export default function Johnstagram() {
         <h1>Welcome to Johnstagram</h1>
         <p>This is the about page.</p>
         
-<Image src="/images/selfie.jpg"
+<Image src="/images/selfie.JPG"
         alt="Hero Image"
         width={1200}
         height={800}/>
 
-        <img src="/images/selfie.jpg" alt="Logo" />
-
-        <image></image>
       </main>
     );
   }
