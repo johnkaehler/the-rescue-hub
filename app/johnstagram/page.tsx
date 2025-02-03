59efa17145ce94f83a10cf2a42a6ba95662c096e
John Kaehler <johnkaehler92@gmail.com>
2025-02-02 17:28:00 -0700

    Created a fuckin about page and uploaded an image

---
+++ app/johnstagram/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+export default function Johnstagram() {
+    return (
+      <main>
+        <h1>Welcome to Johnstagram</h1>
+        <p>This is the about page.</p>
+        
+<Image src="/images/selfie.jpg"
+        alt="Hero Image"
+        width={1200}
+        height={800}/>
+
+        <img src="/images/selfie.jpg" alt="Logo" />
+
+        <image></image>
+      </main>
+    );
+  }
