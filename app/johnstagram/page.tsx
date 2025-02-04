ba8526b8e1c503b5285b698be166145f22605ec4
John Kaehler <johnkaehler92@gmail.com>
2025-02-03 22:15:24 -0700

    Rounded image, playing around with divs

--- app/johnstagram/page.tsx
+++ app/johnstagram/page.tsx
@@ -6,11 +6,18 @@ export default function Johnstagram() {
         <h1>Welcome to Johnstagram</h1>
         <p>This is the about page.</p>
         
-<Image src="/images/selfie.JPG"
-        alt="Hero Image"
-        width={1200}
-        height={800}/>
+        <div className="flex items-center justify-right w-200 h-200">
+        <Image src="/images/selfie.JPG"
+            alt="Hero Image"
+            width={100}
+            height={100}
+            className="rounded-full object-cover"
+            />
+        </div>
 
+        <div className="bg-blue border-white">
+            <h1>Hello</h1>
+        </div>
       </main>
     );
   }
