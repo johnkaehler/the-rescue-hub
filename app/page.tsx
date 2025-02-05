a2ef14eb33b32359701e0748bf096280cb98eded
John Kaehler <johnkaehler92@gmail.com>
2025-02-04 19:21:00 -0700

    Removed apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
-        <p className="mb-2">I'm just tryna save dogs, dawg.</p>
+        <p className="mb-2">Im just tryna save dogs, dawg.</p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
